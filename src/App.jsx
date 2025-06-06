68d7c510a613a853a6f224a45b22d64258c15f34
MoriyaEster <moriyaeo100@gmail.com>
2025-06-06 17:42:38 +0300

    port 5174

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
@@ -12,6 +12,7 @@ function App() {
     <div className='App'>
       <Router basename="/Trello">
         <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/home" element={<Home />} />
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/Trello/',
+  server: {
+    port: 5174
+  },
   plugins: [react()],
 })
